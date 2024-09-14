63a2ffdc5a96cc3a001ac406dc5a8994dd1aa91f
ktzkvin <kevin.kurtz@efrei.net>
2024-09-14 12:14:34 +0200

    🔧WIP

--- dashboard.py
+++ dashboard.py
@@ -3,25 +3,27 @@
 **@email : kevin.kurtz@efrei.net**
 """
 
-# 
+# import libraries
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 import requests  # Pour charger le GeoJSON depuis une URL
-
-st.set_page_config(
-    page_title="Dashboard",
-    page_icon="📊",
-    layout="wide",
-    initial_sidebar_state="expanded")
 
 # Load data
 data_emissions = pd.read_csv('emissions_ges_france.csv')
 data_coordinates = pd.read_csv('communes_coordinates.csv', sep='|')
 dep_data = pd.read_csv('communes-departement-region.csv')
 
+# Set page config
+st.set_page_config(
+    page_title="GHG Emissions - Dashboard",
+    page_icon="🏭",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
 # -------------------------- Clean emissions data -------------------------- #
 data_emissions['Commune_format'] = data_emissions['Commune']  # Clone
+data_emissions['Commune_format'] = data_emissions['Commune_format'].str.title()
 data_emissions['Commune'] = data_emissions['Commune'].str.replace('-', '').str.replace(' ', '').str.replace('\'', '').str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
 
 # ---------------------------- Clean temp data ----------------------------- #
@@ -29,6 +31,7 @@ dep_data = dep_data[['nom_commune_complet', 'nom_departement']]  # Keep only the
 dep_data = dep_data.rename(columns={'nom_departement': 'Département', 'nom_commune_complet': 'Commune'})
 dep_data['Commune'] = dep_data['Commune'].str.upper()
 dep_data['Commune'] = dep_data['Commune'].str.replace('-', '').str.replace(' ', '').str.replace('\'', '').str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+dep_data = dep_data.drop_duplicates(subset='Commune')
 
 # ------------------------- Clean coordinates data ------------------------- #
 data_coordinates['Commune'] = data_coordinates['Commune'].str.upper()
@@ -49,6 +52,10 @@ cols = cols[:1] + [cols[13]] + cols[1:13] + cols[14:]
 data_merged = data_merged[cols]
 data_merged = data_merged.rename(columns={'Commune_format': 'Commune_LF'})
 
+# Create a total column, and convert all columns to numeric
+data_merged[['Agriculture', 'Autres transports', 'Autres transports international', 'CO2 biomasse hors-total', 'Déchets', 'Energie', 'Industrie hors-énergie', 'Résidentiel', 'Routier', 'Tertiaire']] = data_merged[['Agriculture', 'Autres transports', 'Autres transports international', 'CO2 biomasse hors-total', 'Déchets', 'Energie', 'Industrie hors-énergie', 'Résidentiel', 'Routier', 'Tertiaire']].apply(pd.to_numeric, errors='coerce')
+data_merged['Total'] = data_merged[['Agriculture', 'Autres transports', 'Autres transports international', 'CO2 biomasse hors-total', 'Déchets', 'Energie', 'Industrie hors-énergie', 'Résidentiel', 'Routier', 'Tertiaire']].sum(axis=1)
+
 
 # ---------------------------- Sidebar ----------------------------- #
 with st.sidebar:
@@ -58,14 +65,31 @@ with st.sidebar:
 # ---------------------------- Main page ----------------------------- #
 st.title('🏭 Greenhouse Gas Emissions in France')
 
-
-# ---------------------------- App layout ----------------------------- #
 # Columns
-col = st.columns((1.5, 4.5, 2), gap='medium')
+col = st.columns((1.5, 4.5, 2), gap='medium')  
 with col[0]:
     st.write('a')
+
 with col[1]:
     st.write('b')
+
 with col[2]:
-    st.write('z')
+    st.markdown('#### 🥇 Top Communes')
+ 
+    data_merged_sorted = data_merged.sort_values(by='Total', ascending=False)
+
+    st.dataframe(data_merged_sorted,
+                 hide_index=True,
+                 column_order=['Commune_LF', 'Total'],
+                 column_config={
+                     'Commune_LF': st.column_config.TextColumn('Commune'),
+                     'Total': st.column_config.ProgressColumn('Tonne de CO₂eq', format='%.2f', min_value=0, max_value=max(data_merged['Total']))
+                     },
+                 width=800
+                 )
     
+    with st.expander('🔍 Details', expanded=True):
+        st.write('''
+        - Data source: [Inventaire de gaz a effet de serre territorialisé - Data Gouv](https://www.data.gouv.fr/fr/datasets/inventaire-de-gaz-a-effet-de-serre-territorialise/)
+        - :orange[**Note**]: The data is from **2016** and is subject to change.
+        ''')
