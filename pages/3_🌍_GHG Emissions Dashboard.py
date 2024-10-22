997e8d21e2cf63792b8683a5ae9dbc1bd23ebaaa
ktzkvin <kevin.kurtz@efrei.net>
2024-10-22 08:59:15 +0200

    🔧WIP

--- pages/3_🌍_GHG Emissions Dashboard.py
+++ pages/3_🌍_GHG Emissions Dashboard.py
@@ -333,7 +333,7 @@ with col[0]:
 
 # Map and sector breakdown
 with col[1]:
-    st.markdown('####🌍 Emissions and breakdown analysis')
+    st.markdown('#### 🌍 Emissions and breakdown analysis')
     
     # Use Tabs to switch between Map and Brakdown
     tabs = st.tabs(["France Map", "Sector Breakdown","Top 3 Emitters"])
